26253621af064577419faf635b061372d6d4635a
Johan Lopez <johanpppp222345@gmail.com>
2024-09-29 20:54:42 -0500

    codebase

---
+++ src/test/java/com/producto_apps/gestion_financiera/GestionFinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producto_apps.gestion_financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
